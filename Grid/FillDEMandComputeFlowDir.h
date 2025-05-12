48500770ccbd5cbe07bd72be540db5cb5dee7fe1
zhouguiyun-uestc <zhouguiyun@uestc.edu.cn>
2025-05-12 17:00:17 +0800

    Delete Grid directory

--- Grid/FillDEMandComputeFlowDir.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <filesystem>
-#include "grid.h"
-
-void FillDEMandComputeFlowDir(const std::filesystem::path& inputPath, const std::filesystem::path& outputPath);
--- Grid/FlowAccu.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "grid.h"
-//flow accumulation by Zhou et al. 2019
-void computeFlowAccu(Grid<FlowDir>& dirGrid, Grid<unsigned int>& accuGrid);
